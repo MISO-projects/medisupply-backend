d08e0c5183cb47b33f63e6d99c604215f0b38b06
Zamir Narro <josue.narro.a@gmail.com>
2025-10-11 20:21:10 -0500

    feat: add bff web endpoints for vendedores

--- src/bff-web/router/ventas.py
+++ src/bff-web/router/ventas.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 import logging
 
 from services.ventas_service import VentasService, get_ventas_service
+from router.vendedores import vendedor_router
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -12,3 +13,6 @@ ventas_router = APIRouter()
 def health_check(ventas_service: VentasService = Depends(get_ventas_service)):
     return ventas_service.health_check()
 
+# Incluir el router de vendedores como sub-router
+ventas_router.include_router(vendedor_router, prefix="/vendedores", tags=["vendedores"])
+# TODO: router de planes de venta
